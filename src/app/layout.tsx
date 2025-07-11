073c251baa4a1b20934432bf5b5cf32d352f7a16
haaaarsh4 <harshupadhyay2004@gmail.com>
2025-07-11 13:52:46 -0400

    Add icon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,13 @@
 import './globals.css'; // Make sure this points to your CSS file
 
+export const metadata = {
+  title: 'HCNgen',
+  description: 'Random HCN generator',
+  icons: {
+    icon: '/favicon.svg',
+  },
+}
+
 export default function RootLayout({ 
   children 
 }: { 
