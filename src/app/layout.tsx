965bf69dcf2b3814b5c75702a8e3e903355998af
haaaarsh4 <harshupadhyay2004@gmail.com>
2025-07-11 13:55:18 -0400

    Add icon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import './globals.css'; // Make sure this points to your CSS file
 
 export const metadata = {
-  title: 'HCNgen',
+  title: 'Canadian HCN Generator',
   description: 'Random HCN generator',
   icons: {
     icon: '/favicon.svg',
